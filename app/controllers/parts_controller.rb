f8a491255817177103b5f4e33c3bcf8fd12eaa6f
Eldar Avatov <eldar.avatov@gmail.com>
2020-01-01 23:28:46 +0300

    Sizes

--- app/controllers/parts_controller.rb
+++ app/controllers/parts_controller.rb
@@ -64,7 +64,7 @@ class PartsController < ApplicationController
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_part
-      @part = Part.find(key: params[:part])
+      @part = Part.find(id: params[:part])
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
