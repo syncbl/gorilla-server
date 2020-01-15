0ea3f617ffc2353a6e70de7178107858e5a369d5
Eldar Avatov <deadalice@gmail.com>
2020-01-16 00:18:27 +0200

    Authorize only with endpoint

--- app/controllers/auth/sessions_controller.rb
+++ app/controllers/auth/sessions_controller.rb
@@ -8,8 +8,13 @@
         sign_in(resource_name, resource)
 
         @endpoint = Endpoint.find_by(user: resource, key: params[:user][:endpoint]) ||
-                    Endpoint.create(user: resource)
-
+                    Endpoint.new(user: resource)
+        if @endpoint.new_record?
+          @endpoint.save
+          @endpoint.reload
+        else
+          @endpoint.regenerate_authentication_token
+        end
         respond_with_authentication_token(resource)
       end
     end
@@ -21,7 +26,7 @@
     render json: {
       version: Rails.application.config.api_version,
       session: {
-        endpoint: @endpoint.reload.key,
+        endpoint: @endpoint.key,
         token: @endpoint.authentication_token
       }
     }
