63484711a0e531d75b9367cf969c2fcf2a50e105
Eldar Avatov <deadalice@gmail.com>
2020-01-21 19:01:33 +0200

    jQuery fix

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,8 +3,10 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
+
 require("@rails/ujs").start()
 require("turbolinks").start()
+require("jquery")
 require("@rails/activestorage").start()
 require("channels")
 require("bootstrap/dist/js/bootstrap")
--- install.sh
+++ install.sh
@@ -5,6 +5,6 @@ sudo -u postgres bash -c "psql -c \"CREATE USER gpuser WITH PASSWORD 'vatruchuji
 sudo -u postgres bash -c "psql -c \"ALTER USER gpuser WITH SUPERUSER;\""
 sudo apt-get update && sudo apt-get install yarn
 yarn install --check-files
-yarn add bootstrap jquery popper.js
+yarn add jquery popper.js bootstrap
 bundle install
 rails db:create db:migrate db:seed assets:precompile
