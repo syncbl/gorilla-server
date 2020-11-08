794945c2ae0d4b48d6c8d98b2dbf2feef5b6562d
Eldar Avatov <eldar.avatov@gmail.com>
2020-11-08 02:25:09 +0200

    Deploy

--- config/initializers/constants.rb
+++ config/initializers/constants.rb
@@ -3,3 +3,4 @@ NAME_EXCLUSIONS = %w(user users package packages setting settings
 NAME_FORMAT = /\A[A-Za-z\d\-\_]*\z/
 EMAIL_FORMAT = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
 ENDPOINT_TOKEN_REGEN_RANDOM = 10
+MAX_PACKAGE_SIZE = 5.gigabytes
