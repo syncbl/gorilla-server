4c298b71b8b63ba479f9779f7c1da0090b028201
Eldar Avatov <eldar.avatov@gmail.com>
2021-05-12 01:14:52 +0300

    Deploy

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -28,6 +28,14 @@ class ApplicationController < ActionController::Base
     end
   end
 
+  def render_json_error(messages, status:)
+    if messages.is_a? Array
+      render json: { errors: messages }, status: status
+    else
+      render json: { error: messages }, status: status
+    end
+  end
+
   private
 
   def api_check_headers
