91328ef42b7edae81b9572f9537626bba8d452ff
Eldar Avatov <eldar.avatov@gmail.com>
2021-05-23 16:06:40 +0300

    Deploy

--- app/jobs/application_job.rb
+++ app/jobs/application_job.rb
@@ -1,5 +1,4 @@
 class ApplicationJob < ActiveJob::Base
-  # TODO: Learn about jobs
   # Automatically retry jobs that encountered a deadlock
   # retry_on ActiveRecord::Deadlocked
   # Most jobs are safe to ignore if the underlying records are no longer available
