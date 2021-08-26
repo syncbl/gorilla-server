c415f030463196532ad2fade5260f3d58d1e8a9f
Eldar Avatov <eldar.avatov@gmail.com>
2021-08-26 20:34:44 +0300

    Deploy

--- config/initializers/mini_profiler.rb
+++ config/initializers/mini_profiler.rb
@@ -1,2 +1,4 @@
-Rack::MiniProfiler.config.position = 'bottom-left'
-Rack::MiniProfiler.config.show_total_sql_count = true
+if Rails.env.development?
+  Rack::MiniProfiler.config.position = "bottom-left"
+  Rack::MiniProfiler.config.show_total_sql_count = true
+end
