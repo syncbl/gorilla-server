bb30554fc81279bf9a4c9f38d69c584021bb2727
Eldar Avatov <eldar.avatov@gmail.com>
2021-08-30 12:47:50 +0300

    Deploy

--- app/models/concerns/notifiable.rb
+++ app/models/concerns/notifiable.rb
@@ -1,6 +1,7 @@
 module Notifiable
   extend ActiveSupport::Concern
   require "redis"
+  require "connection_pool"
 
   def notify_object(method, object)
     # Notifications can be one per object or one per activity in order to avoid spam
@@ -9,21 +10,27 @@ module Notifiable
 
   def notifications
     messages = Set[]
-    redis_connection.scan_each(match: "N_#{self.id}.*") do |key|
-      messages << redis_connection.hgetall(key)
-      redis_connection.del(key)
+    redis_pool.with do |redis|
+      redis.scan_each(match: "N_#{self.id}.*") do |key|
+        messages << redis.hgetall(key)
+        redis.del(key)
+      end
     end
     messages.to_a
   end
 
   private
 
-  def redis_connection
-    @redis_connection ||= Redis.new(url: ENV.fetch("REDIS_URL") { "redis://localhost:6379/1" })
+  def redis_pool
+    @redis_pool ||= ConnectionPool.new(size: ActiveRecord::Base.connection_pool.db_config.pool) do
+      Redis.new(url: ENV.fetch("REDIS_URL") { "redis://localhost:6379/1" })
+    end
   end
 
   def deliver_notification(key, value)
-    redis_connection.mapped_hmset key, value
-    redis_connection.expire key, NOTIFICATION_EXPIRES_IN
+    redis_pool.with do |redis|
+      redis.mapped_hmset key, value
+      redis.expire key, NOTIFICATION_EXPIRES_IN
+    end
   end
 end
