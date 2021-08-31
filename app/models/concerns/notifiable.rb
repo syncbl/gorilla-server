dd21e38e11f262c39eb6ab2ad8da4755034f236c
Eldar Avatov <eldar.avatov@gmail.com>
2021-09-01 00:06:41 +0300

    Deploy

--- app/models/concerns/notifiable.rb
+++ app/models/concerns/notifiable.rb
@@ -9,8 +9,8 @@ module Notifiable
 
   def notifications
     messages = Set[]
-    redis_connection.with do |redis|
-      redis.scan_each(match: "N_#{self.id}.*") d-=5o |key|
+    redis_pool.with do |redis|
+      redis.scan_each(match: "N_#{self.id}.*") do |key|
         value = redis.hgetall(key)
         messages << value if validate_notification(key, value)
         redis.del(key)
@@ -21,13 +21,13 @@ module Notifiable
 
   private
 
-  def redis_connection
-    @redis_connection ||= Api::Redis.new.pool
+  def redis_pool
+    @redis_pool ||= Api::Redis.new.pool
   end
 
   def deliver_notification(key, value)
     if validate_notification(key, value)
-      redis_connection.with do |redis|
+      redis_pool.with do |redis|
         redis.mapped_hmset key, value
         redis.expire key, NOTIFICATION_EXPIRES_IN
       end
