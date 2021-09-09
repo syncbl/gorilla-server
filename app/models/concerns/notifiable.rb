2d3d678691c05a5e2787746a25024f6a0e5d8e15
Eldar Avatov <eldar.avatov@gmail.com>
2021-09-09 18:47:45 +0300

    Deploy

--- app/models/concerns/notifiable.rb
+++ app/models/concerns/notifiable.rb
@@ -1,11 +1,11 @@
 module Notifiable
   extend ActiveSupport::Concern
 
-  def notify(method, object)
+  def notify(method, object, payload = nil)
     # Notifications can be one per object or one per activity in order to avoid spam
     notification = object.is_a?(ApplicationRecord) ? object.id : object
-    deliver_notification "N_#{self.id}.#{notification}",
-                         Hash[method, notification]
+    payload ||= Hash[method, notification]
+    deliver_notification "N_#{self.id}.#{notification}", payload
   end
 
   def notifications
@@ -13,7 +13,7 @@ module Notifiable
     redis_pool.with do |redis|
       redis.scan_each(match: "N_#{self.id}.*") do |key|
         value = redis.hgetall(key)
-        messages << value if validate_notification(key, value)
+        messages << value if validate_notification(value)
         redis.del(key)
       end
     end
@@ -27,7 +27,7 @@ module Notifiable
   end
 
   def deliver_notification(key, value)
-    if validate_notification(key, value)
+    if validate_notification(value)
       redis_pool.with do |redis|
         redis.mapped_hmset key, value
         redis.expire key, NOTIFICATION_EXPIRES_IN
@@ -35,7 +35,7 @@ module Notifiable
     end
   end
 
-  def validate_notification(key, value)
-    %i[add_package remove_package].include?(key) && value.size > 0
+  def validate_notification(payload)
+    payload.size > 0 && %w[add_package remove_package].include?(payload.keys[0])
   end
 end
