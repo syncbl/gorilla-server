7616002f273b00cbf123275cb905ee1cb7eef680
Eldar Avatov <eldar.avatov@gmail.com>
2021-10-15 18:16:04 +0300

    Deploy

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,21 +1,3 @@
 const { environment } = require('@rails/webpacker')
 
-const webpack = require('webpack')
-environment.plugins.prepend('Provide',
-  new webpack.ProvidePlugin({
-    $: 'jquery/src/jquery',
-    jQuery: 'jquery/src/jquery'
-  })
-)
-
-// Get the actual sass-loader config
-const sassLoader = environment.loaders.get('sass')
-const sassLoaderConfig = sassLoader.use.find(function(element) {
-  return element.loader == 'sass-loader'
-})
-
-// Use Dart-implementation of Sass (default is node-sass)
-const options = sassLoaderConfig.options
-options.implementation = require('sass')
-
 module.exports = environment
