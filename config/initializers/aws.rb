4821993ec5cf953a576fa97a3ebc555c7edd2b23
Eldar Avatov <eldar.avatov@gmail.com>
2021-12-08 17:34:21 +0200

    Deploy

--- config/initializers/aws.rb
+++ config/initializers/aws.rb
@@ -3,4 +3,4 @@
 #  region: 'eu-central-1',
 #  access_key_id: 'AKIAWUBHPOTDUYFQJBBD',
 #  secret_access_key: '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'
-#).bucket('gp-bucket-1')
+#).bucket('syncbl-bucket-1')
