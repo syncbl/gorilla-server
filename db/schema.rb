4503a5abf1ec6fb392198c70bca9780c6e6cf4aa
Eldar Avatov <eldar.avatov@gmail.com>
2022-03-03 23:58:49 +0200

    Deploy

--- db/schema.rb
+++ db/schema.rb
@@ -24,7 +24,7 @@ ActiveRecord::Schema[7.0].define(version: 20_220_216_150_508) do
     t.datetime "created_at", precision: nil, null: false
     t.index ["blob_id"], name: "index_active_storage_attachments_on_blob_id"
     t.index %w[record_type record_id name blob_id], name: "index_active_storage_attachments_uniqueness",
-                                                    unique: true
+ unique: true
   end
 
   create_table "active_storage_blobs", force: :cascade do |t|
