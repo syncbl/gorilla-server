d531f5555a3f7b33ca0538e94d5523de338590cc
Eldar Avatov <eldar.avatov@gmail.com>
2022-05-18 00:17:31 +0300

    Deploy

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -4,42 +4,15 @@ class Ability
   include CanCan::Ability
 
   def initialize(user)
-    can :read, Package
+    can :read, Package, blocked_at: nil
     cannot :read, Package, published_at: nil
 
-    return if user.blank?
+    return if user.blank? || user.blocked?
 
     can :read, User
     can :update, User, user: user
-    can :create, Package
-    can :update, Package, user: user
-    can :destroy, Package, user:
-
-    # Define abilities for the passed in user here. For example:
-    #
-    #   user ||= User.new # guest user (not logged in)
-    #   if user.admin?
-    #     can :manage, :all
-    #   else
-    #     can :read, :all
-    #   end
-    #
-    # The first argument to `can` is the action you are giving the user
-    # permission to do.
-    # If you pass :manage it will apply to every action. Other common actions
-    # here are :read, :create, :update and :destroy.
-    #
-    # The second argument is the resource the user can perform the action on.
-    # If you pass :all it will apply to every resource. Otherwise pass a Ruby
-    # class of the resource.
-    #
-    # The third argument is an optional hash of conditions to further filter the
-    # objects.
-    # For example, here the user can only update published articles.
-    #
-    #   can :update, Article, :published => true
-    #
-    # See the wiki for details:
-    # https://github.com/CanCanCommunity/cancancan/wiki/Defining-Abilities
+    can :manage, Package, user: user, blocked_at: nil
+    can :manage, Source, user: user
+    can :manage, Setting, user:
   end
 end
