2faf9a80cf427cf6c43c1bfabec696e27ed4f557
Eldar Avatov <eldar.avatov@gmail.com>
2022-05-22 17:10:32 +0300

    Fixed abilities

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -7,35 +7,28 @@ class Ability
     can :read, Package, blocked_at: nil
     cannot :read, Package, published_at: nil
 
+    can :destroy, Setting,
+        endpoint: {
+          user:
+        }
+
     return if user.blank? || user.blocked?
 
     can :read, User
     can :update, User, id: user.id
-    can :manage, Endpoint, user: user,
-                           blocked_at: nil
+    can :manage, Endpoint, user: user
     can :destroy, Endpoint, user: user
-    can :manage, Package, user: user,
-                          blocked_at: nil
+    can :manage, Package, user: user
     can :destroy, Package, user: user
     can :manage, Source,
         package: {
-          user:,
-          blocked_at: nil,
-        },
-        blocked_at: nil
+          user:
+        }
     can :destroy, Source,
         package: {
           user:
         }
     can :manage, Setting,
-        endpoint: {
-          user:,
-          blocked_at: nil
-        },
-        package: {
-          blocked_at: nil
-        }
-    can :destroy, Setting,
         endpoint: {
           user:
         }
