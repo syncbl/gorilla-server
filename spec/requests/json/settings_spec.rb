2428b97f49de70e495b63104375c9ba9a98ff06f
Eldar Avatov <eldar.avatov@gmail.com>
2022-06-16 01:21:54 +0300

    Fixing settings

--- spec/requests/json/settings_spec.rb
+++ spec/requests/json/settings_spec.rb
@@ -2,10 +2,10 @@ require "rails_helper"
 
 RSpec.describe "Settings", type: :request do
   let!(:user) { create(:user1) }
-  let!(:endpoint) { create(:endpoint1, user: user) }
-  let!(:bundle1) { create(:bundle1, user: user) }
-  let!(:component1) { create(:component1, user: user) }
-  let!(:component2) { create(:component2, user: user) }
+  let!(:endpoint) { create(:endpoint1, user:) }
+  let!(:bundle1) { create(:bundle1, user:) }
+  let!(:component1) { create(:component1, user:) }
+  let!(:component2) { create(:component2, user:) }
   # let!(:bundle2) { create(:bundle2, user:) }
   let!(:source1) { create(:source1, package: bundle1) }
   let!(:source2) { create(:source2, package: bundle1) }
@@ -45,8 +45,8 @@ RSpec.describe "Settings", type: :request do
     # TODO: fix this
     it "renders a successful response" do
       get endpoint_settings_path(current_endpoint: endpoint, format: :json), params: {
-                                                                               packages: source1.id.to_s,
-                                                                             }
+        packages: source1.id.to_s,
+      }
       expect(response).to have_http_status(:ok)
       expect(JSON.parse(response.body, symbolize_names: true)).to match(valid_response)
     end
@@ -114,8 +114,8 @@ RSpec.describe "Settings", type: :request do
       # TODO: fix this
       it "renders a successful response" do
         post endpoint_settings_path(current_endpoint: endpoint, format: :json), params: {
-                                                                                  packages: [component1.id, component2.id],
-                                                                                }
+          packages: [component1.id, component2.id],
+        }
         expect(response).to have_http_status(:accepted)
         expect(JSON.parse(response.body, symbolize_names: true)).to match(valid_response)
       end
@@ -149,8 +149,8 @@ RSpec.describe "Settings", type: :request do
       # TODO: fix this
       it "renders a successful response" do
         post endpoint_settings_path(current_endpoint: endpoint, format: :json), params: {
-                                                                                  user_id: component2.user.name, package_id: component2.name.to_s,
-                                                                                }
+          user_id: component2.user.name, package_id: component2.name.to_s,
+        }
         expect(response).to have_http_status(:accepted)
         expect(JSON.parse(response.body, symbolize_names: true)).to match(valid_response)
       end
