351b4fca0f59a9b7cbd4b4da6f7c70d1b63bc322
Eldar Avatov <eldar.avatov@gmail.com>
2022-06-18 02:12:07 +0300

    Fixing Settings

--- db/schema.rb
+++ db/schema.rb
@@ -28,13 +28,13 @@ ActiveRecord::Schema[7.0].define(version: 2022_02_16_150508) do
 
   create_table "active_storage_blobs", force: :cascade do |t|
     t.string "key", null: false
+    t.string "service_name", null: false
     t.string "filename", null: false
     t.string "content_type"
     t.text "metadata"
     t.bigint "byte_size", null: false
     t.string "checksum", null: false
     t.datetime "created_at", precision: nil, null: false
-    t.string "service_name", null: false
     t.index ["checksum"], name: "index_active_storage_blobs_on_checksum"
     t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
   end
